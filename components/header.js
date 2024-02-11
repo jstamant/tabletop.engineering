1ebdd598f9b38cbf4c524be506db7abf3a4a05c1
Justin R. St-Amant <jstamant24@gmail.com>
2024-02-11 13:52:00 -0600

    Improved header styling

--- components/header.js
+++ components/header.js
@@ -5,13 +5,13 @@ import { faDiceD20 } from '@fortawesome/free-solid-svg-icons'
 
 export default function Header () {
   return (
-    <header className="bg-neutral-200 h-16 px-8 flex items-center justify-between">
-      <Link href="/" className="text-2xl font-bold"><FontAwesomeIcon icon={faDiceD20} className="mr-2" />Tabletop.Engineering</Link>
-      <nav className="text-lg">
-        <Link href="/">Home</Link>
-        <Link href="/posts">Posts</Link>
-        <Link href="/about">About</Link>
-        <Link href="/contact">Contact</Link>
+    <header className="bg-neutral-200 h-16 px-8 flex items-center justify-between font-semibold">
+      <Link href="/" className="text-2xl"><FontAwesomeIcon icon={faDiceD20} className="mr-2" />Tabletop.Engineering</Link>
+      <nav className="text-base flex">
+        <Link className="p-4" href="/">Home</Link>
+        <Link className="p-4" href="/posts">Posts</Link>
+        <Link className="p-4" href="/about">About</Link>
+        <Link className="p-4" href="/contact">Contact</Link>
       </nav>
     </header>
   )
