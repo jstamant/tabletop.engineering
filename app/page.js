2301491d19e3be669d629d74c42d682894b34edf
Justin R. St-Amant <jstamant24@gmail.com>
2024-02-12 16:42:00 -0600

    Added component for post summaries

--- app/page.js
+++ app/page.js
@@ -1,8 +1,11 @@
+import PostCard from '../components/postcard.js'
+
 export default function Page() {
   return (
     <>
-      <div>
+      <div className="mx-auto w-11/12 md:w-2/3">
         <h1>Sample header in page</h1>
+        <PostCard />
       </div>
     </>
   )
---
+++ components/postcard.js
@@ -0,0 +1,3 @@
+export default function PostCard() {
+  return <div>Sample PostCard</div>
+}
