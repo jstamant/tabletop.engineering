49f83b2c022d43453bcf013cdb153c6164745313
Justin R. St-Amant <jstamant24@gmail.com>
2024-02-16 09:48:00 -0600

    Started populating and styling post summary cards

--- app/posts/page.js
+++ app/posts/page.js
@@ -1,12 +1,14 @@
+import PostCard from '../../components/postcard';
+
 import { getAllPosts } from '../../lib/api';
 
 export default function Page() {
   // TODO need to get a list of the first 10 posts
   let allPosts = getAllPosts();
-  allPosts = allPosts.map((post, idx) => <p key={idx}>{post.title}</p>);
+  allPosts = allPosts.map((post, idx) => <PostCard slug={post.slug} key={post.slug} />);
   return (
     <>
-      <h1>List of all posts:</h1>
+      <header className="text-2xl font-bold my-4">Posts</header>
       {allPosts}
     </>
   );
--- components/postcard.js
+++ components/postcard.js
@@ -1,3 +1,14 @@
-export default function PostCard() {
-  return <div>Sample PostCard</div>
+import Link from 'next/link';
+
+import { getPostBySlug } from '../lib/api';
+
+export default function PostCard(params) {
+  const post = getPostBySlug(params.slug);
+  return (
+    <article className="bg-neutral-200 my-4 p-4">
+      <header className="font-bold">{post.title}</header>
+      <p className="my-2">{post.summary}</p>
+      <Link href={"/posts/" + post.slug}>Read more...</Link>
+    </article>
+  );
 }
