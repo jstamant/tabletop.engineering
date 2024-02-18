39ca1ee7f47953e5cd6a8c0a299f1afcd6e856a2
Justin R. St-Amant <jstamant24@gmail.com>
2024-02-18 11:09:00 -0600

    Added and styled advisory component

--- app/page.js
+++ app/page.js
@@ -1,6 +1,9 @@
+import Advisory from '../components/advisory';
+
 export default function Page() {
   return (
     <>
+      <Advisory />
       <h1>Sample header in page</h1>
     </>
   )
--- components/advisory.js
+++ components/advisory.js
@@ -1,14 +1,10 @@
+import Link from 'next/link';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons'
+
 export default function Advisory () {
-  return <p>Some advisory text</p>
+  return (
+    <aside className="my-4 p-4 bg-neutral-100 border border-neutral-200">I'm currently looking for a group to play in. I may also be willing to run a game for a small group of players, but we'll see. See the <Link href="/contact" className="font-bold hover:underline">contact page<FontAwesomeIcon icon={faArrowUpRightFromSquare} className="ml-1 text-base" /></Link> more info.</aside>
+  );
 }
-
-/*
----
-title: Advisory
-date: 2021-08-21
----
-
-I'm currently looking for a group to play in. I may also be willing to
-run a game for a small group of players, but we'll see. See the
-[contact page](/contact) for more info.
-*/
