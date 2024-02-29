1493f31b1fb2150ee1a2dcd2baa1c63965277c01
Justin R. St-Amant <jstamant24@gmail.com>
2024-02-29 12:41:10 -0600

    Added footer link

--- components/footer.js
+++ components/footer.js
@@ -1,7 +1,12 @@
+import Link from 'next/link';
+
 export default function Footer () {
   return (
-    <footer className="text-white bg-neutral-900 py-8 text-center">
-      <p>&copy; 2021-2024 Justin St-Amant - All rights reserved</p>
+    <footer className="text-xs text-white bg-neutral-900 py-8">
+      <div className="w-11/12 md:w-2/3 mx-auto flex justify-between">
+        <p>&copy; 2021-2024 Justin St-Amant - All rights reserved</p>
+        <Link href="/" className="hover:underline">Home</Link>
+      </div>
     </footer>
   )
 }
